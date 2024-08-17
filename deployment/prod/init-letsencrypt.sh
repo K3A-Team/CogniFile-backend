323b50f0e57c25597ba406babf9aea7b0b2b7394
ELHart05 <o.allaoua@esi-sba.dz>
2024-08-17 15:02:45 +0100

    migrate to ssl certifcate generation

--- deployment/prod/init-letsencrypt.sh
+++ deployment/prod/init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(api.cognifile.org)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="allaouaokba@gmail.com"
-staging=1
+staging=0
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
