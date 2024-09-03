a05c953dd661715cac08e3d703bd63a98db430e5
ELHart05 <o.allaoua@esi-sba.dz>
2024-09-03 04:37:55 +0100

    added reset and forget logic

---
+++ code/Models/Entities/PasswordResetTokens.py
@@ -0,0 +1,19 @@
+import uuid
+
+class PasswordResetTokens:
+    """
+    Represents a password reset token entity in the file management system.
+    """
+    def __init__(self, random_value: str, expires_at: str, email: str, id : str = None):
+        self.id = id or str(uuid.uuid4())
+        self.random_value = random_value
+        self.expires_at = expires_at
+        self.email = email
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "random_value": self.random_value,
+            "email": self.email,
+            "expires_at": self.expires_at
+        }
